c977372cada09985fb36804700353cdf0b698dfc
U028279 <jose.ordas.ext@rsi.cajarural.com>
2022-05-19 10:29:23 +0200

    Añadidos ejemplos y ejercicios streams

--- TallerProgramacionFuncionalSerbatic/Taller Programacion Funcional/src/main/java/c_introduccion_streams/EjemplosDeStreams.java
+++ TallerProgramacionFuncionalSerbatic/Taller Programacion Funcional/src/main/java/c_introduccion_streams/EjemplosDeStreams.java
@@ -3,6 +3,7 @@ package c_introduccion_streams;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import b_interfascesFuncionales.Mock;
 import personas.Person;
 
@@ -13,15 +14,21 @@ public class EjemplosDeStreams {
     public static void main(String[] args) {
         List<Person> list =  Mock.getPeople();
         
-        //ejemplo1Clasico(list);
-        //ejemplo1Stream(list);
-        
-        //ejemplo2Clasico(list);
-        //ejemplo2Stream(list);
-        
+//        ejemplo1Clasico(list);
+//        ejemplo1Stream(list);
+//        
+//        ejemplo2Clasico(list);
+//        ejemplo2Stream(list);
+//        
+//        ejemploFiltrarYgenerarNuevaListaClasico(list);
+//        
+//        List<Person> nuevaList0 =ejemploFiltrarYgenerarNuevaListaStream(list);
+//        List<Person> nuevaList1 = ejemploFiltrarYgenerarNuevaListaStream(list,p->p.getName().startsWith("A"));
+//        List<Person> nuevaList2 = ejemploFiltrarYgenerarNuevaListaStream(list,p->p.getName().startsWith("T"));
+//        List<Person> nuevaList3 = ejemploFiltrarYgenerarNuevaListaStream(list,p->p.getName().startsWith("T") && p.getAge()<50);
     }
     
-    
+
     private static void ejemplo1Clasico(List<Person> lista) {
         for(Person p : lista) {
             System.out.println(p);
@@ -31,6 +38,30 @@ public class EjemplosDeStreams {
     private static void ejemplo1Stream(List<Person> lista) {
         lista.stream().forEach(p -> System.out.println(p));
     }    
+    
+    
+    
+    private static List<Person> ejemploFiltrarYgenerarNuevaListaClasico(List<Person> lista) {
+        List<Person> nuevaLista = new ArrayList<>();
+        for(Person p : lista) {
+            if(p.getName().startsWith("A")) {
+                nuevaLista.add(p);
+            }
+        }
+        return nuevaLista;
+    }
+    
+    private static List<Person> ejemploFiltrarYgenerarNuevaListaStream(List<Person> lista) {
+        return lista.stream().filter(p->p.getName().startsWith("A")).collect(Collectors.toList());
+    }
+    
+    private static List<Person> ejemploFiltrarYgenerarNuevaListaStream(List<Person> lista,Predicate<Person> pred) {
+        return lista.stream().filter(pred).collect(Collectors.toList());
+    }
+    
+    
+    
+    
     
     private static void ejemplo2Clasico(List<Person> lista) {
         List<Person> nuevaLista = new ArrayList<>();
@@ -42,18 +73,21 @@ public class EjemplosDeStreams {
         }
     }
     
-    private static void ejemplo2Stream(List<Person> lista) {
+    private static List<Person> ejemplo2Stream(List<Person> lista) {
         List<Person> nuevaLista = new ArrayList<>();
         Function<Person,String> miFunc =p -> {
                                                 nuevaLista.add(p);
                                                 return p.getName();
                                                 };
         Predicate<Person> miPred = (p) -> p.getName().startsWith("A");      
+        
         lista.stream().filter(miPred).map(miFunc).forEach(x -> System.out.println(x));
         
-        lista.stream().filter(p -> p.getName().startsWith("A"))
-                      .map(p -> {nuevaLista.add(p);return p.getName();})
-                      .forEach(x -> System.out.println(x));
+        
+        //Sin crear por separado el predicate y el mifunc
+        lista.stream().filter(p -> p.getName().startsWith("A")).map(p -> {nuevaLista.add(p);return p.getName();}).forEach(x -> System.out.println(x));
+        
+        return nuevaLista;
         }
     }
 
--- TallerProgramacionFuncionalSerbatic/Taller Programacion Funcional/src/main/java/c_introduccion_streams/EjercicioStream1.java
+++ TallerProgramacionFuncionalSerbatic/Taller Programacion Funcional/src/main/java/c_introduccion_streams/EjercicioStream1.java
@@ -7,24 +7,38 @@ import personas.Person;
 public class EjercicioStream1 {
     
     public static void main(String[] args) {
+        List<Fichero> listaNombresFicheros = new ArrayList<>();
         listaNombresFicheros.add(new Fichero("fichero_1645461645.txt",1111,new Person("jose",34,Gender.MALE)));
         listaNombresFicheros.add(new Fichero("fichero_1045441572.txt",7894,new Person("juan",22,Gender.MALE)));
         listaNombresFicheros.add(new Fichero("fichero_1142363563.txt",1234,new Person("pedro",25,Gender.MALE)));
         listaNombresFicheros.add(new Fichero("fichero_1385462869.txt",5324,new Person("marco",59,Gender.MALE)));
         listaNombresFicheros.add(new Fichero("fichero_1365462860.txt",6323,new Person("pedro",12,Gender.MALE)));
         listaNombresFicheros.add(new Fichero("fichero_1945468963.txt",2322,new Person("arturo",19,Gender.MALE)));
-        listaNombresFicheros.add(new Fichero("fichero_1245468963.txt",2322,new Person("Maria",19,Gender.FEMALE)));
+        listaNombresFicheros.add(new Fichero("fichero_1245468963.txt",2322,new Person("maria",19,Gender.FEMALE)));
         listaNombresFicheros.add(new Fichero("fichero_1315468963.txt",2322,new Person("catalina",91,Gender.FEMALE)));
+        listaNombresFicheros.add(new Fichero("fichero_1815468963.txt",2322,new Person("pepa",51,Gender.FEMALE)));
+        
+        
+        
+        //Sacar por consola el nombre todas las mujeres  cuyo fichero tenga un timeStamp menor que 1400000000  
 
+        //Obtener una lista nueva cambiandoles el genero a todos las personas cuyo nomre empiece con "p"
         
         
+        //Aumentar en 200 el sueldo solo de las personas cuya edad sea menor que 25 y mostrar por pantalla sus "nombre - sueldo"
         
-        //Sacar por consola todas las mujeres  cuyo fichero tenga un timeStamp menor que 1400000000  
+        
+        //Sacar por pantalla la lista ordenada por nombre ascendente
+        
+        
+        //Sacar por pantalla la lista ordenada por nombre descendente
+        
+        //Sacar por pantalla la lista ordenada por edad ascendente
+        
         
         //Obtener  el ficheros cuyo propietario tenga mas edad
 
-        //Obtener una lista cambiandoles el genero a todos las personas cuyo nomre empiece con "p"
-
+        
     }
 
 }
@@ -42,4 +56,30 @@ class Fichero{
         this.tamaño=tamaño;
         this.propietario = propietario;
     }
+
+    
+    public String getNombre_timeStamp() {
+        return nombre_timeStamp;
+    }
+
+    public void setNombre_timeStamp(String nombre_timeStamp) {
+        this.nombre_timeStamp = nombre_timeStamp;
+    }
+    
+    public int getTamaño() {
+        return tamaño;
+    }
+    
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public Person getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Person propietario) {
+        this.propietario = propietario;
+    }
+    
 }
